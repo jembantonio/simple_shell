f5d1f2f083b992a5ef4a394c6ed2172cc6a4df83
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-04-09 13:16:32 -0700

    code change

--- shell.h
+++ shell.h
@@ -20,6 +20,7 @@ int exec_cmd(char **argv, char **envp);
 int _strcmp(char *s1, const char *s2, int n);
 void _strcpy(char *dest, const char *src, size_t n);
 char *_strdup(const char *s);
+char *_getenv(char **env, char *key);
 
 
 #endif
