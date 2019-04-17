3257b34fd6937ec973455c87f8183fc7bc897173
Your Name <you@example.com>
2019-04-17 18:18:05 +0000

    added case for execve == -1

--- execute.c
+++ execute.c
@@ -20,12 +20,20 @@ int run_cmd(const char *path, char **argv, char **env)
 	if (pid == -1)
 	{
 		perror("Error:");
-		exit(1);
+		free(argv);
+		free(env);
+		return (1);
 	}
 
 	if (pid == 0)
 	{
-		execve(path, argv, env);
+		if (execve(path, argv, env) == -1)
+		{
+			perror("Error:");
+			free(argv);
+			free(env);
+			exit(0);
+		}
 	}
 
 	/* parent */
